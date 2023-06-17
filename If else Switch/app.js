26f2316c579b563c021339b735e1cd3803c469a0
Sagor Mondol <130614802+Sagor-Mondol@users.noreply.github.com>
2023-06-17 07:37:42 +0600

    Add files via upload

---
+++ If else Switch/app.js
@@ -0,0 +1,56 @@
+
+// Switch Use
+// switch , case, default
+
+var myName = prompt();
+
+switch (myName){
+  case 'Mondol Sagor':
+    console.log('Your Name is Last Name and Fast Name');
+    break;
+
+    case 'Sagor Mondol':
+    console.log('Your Name is Full Name');
+    break;
+    
+    case ' Mondol':
+     console.log('Your Name is  Last Name');
+    break;
+
+      case 'Sagor':
+      console.log('Your Name is Fast Name');
+      break;
+
+      case 'Zaddi Baba':
+      console.log('Your Name is Nick Name');
+      break;
+
+      case 'Abir':
+      console.log('Your Name is Fack Name');
+      break;
+    default:
+      console.log('Plz apnar sothik Name ta likhon');
+}
+
+/*
+// if eles if eles
+var myName = prompt();
+
+if (myName == 'Sagor Mondol'){
+  console.log('Your Full Name');
+}
+else if ( myName == 'Mondol Sagor'){
+  console.log('Your Last Name and Fast Name');
+}
+else if ( myName == 'Sagor'){
+  console.log('Your and Fast Name');
+}
+else if ( myName == 'Mondol'){
+  console.log('Your Last Name');
+}
+else if ( myName == 'Zaddi Baba'){
+  console.log('Your Fack Name');
+} else{
+  console.log('Apnar Sothik Number ti likon');
+}
+*/
